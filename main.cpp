967fcb50465c229bd072a71b22fd312757f35fa4
ip61-ua <ip61@alu.ua.es>
2026-02-13 08:54:52 +0100

    test: example raylib

--- main.cpp
+++ main.cpp
@@ -1,116 +1,32 @@
 #include <raylib.h>
 
-typedef enum
-{
-  LOGO = 0,
-  TITLE,
-  GAMEPLAY,
-  ENDING
-} GameScreen;
-
-GameScreen currentScreen = LOGO;
-
-int framesCounter = 0;
-
 int
 main (void)
 {
-  const int screenHeight = 800, screenWidth = 600;
+  const int screenHeight = 600, screenWidth = 800;
   InitWindow (screenWidth, screenHeight, "Hola desde raylib");
 
   SetTargetFPS (60);
 
-  Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
-  Color ballColor = DARKBLUE;
+  float squareSide = 70.0f;
+  Vector2 squarePositionCenter
+      = { (float)screenWidth / 2 - (float)squareSide / 2,
+          (float)screenHeight / 2 - (float)squareSide / 2 };
+  float factorSpeed = 5.0f;
 
   while (!WindowShouldClose ())
     {
-      // if (IsKeyDown (KEY_RIGHT))
-      //   ballPosition.x += 3.0f;
-      // if (IsKeyDown (KEY_LEFT))
-      //   ballPosition.x -= 3.0f;
-      // if (IsKeyDown (KEY_UP))
-      //   ballPosition.y -= 3.0f;
-      // if (IsKeyDown (KEY_DOWN))
-      //   ballPosition.y += 3.0f;
-
-      // if (IsGamepadAvailable (0))
-      //   {
-      //     if (IsGamepadButtonDown (0, GAMEPAD_BUTTON_LEFT_FACE_RIGHT))
-      //       ballPosition.x += 3.0f;
-      //     if (IsGamepadButtonDown (0, GAMEPAD_BUTTON_LEFT_FACE_LEFT))
-      //       ballPosition.x -= 3.0f;
-      //     if (IsGamepadButtonDown (0, GAMEPAD_BUTTON_LEFT_FACE_UP))
-      //       ballPosition.y -= 3.0f;
-      //     if (IsGamepadButtonDown (0, GAMEPAD_BUTTON_LEFT_FACE_DOWN))
-      //       ballPosition.y += 3.0f;
-      //   }
-
-      ballPosition = GetMousePosition ();
-
-      if (IsMouseButtonDown (MOUSE_BUTTON_LEFT))
-        ballColor = YELLOW;
-      else if (IsMouseButtonDown (MOUSE_BUTTON_MIDDLE))
-        ballColor = LIME;
-      else if (IsMouseButtonDown (MOUSE_BUTTON_RIGHT))
-        ballColor = RED;
-
-      switch (currentScreen)
-        {
-        case LOGO:
-          framesCounter++;
-          if (framesCounter > 300)
-            currentScreen = TITLE;
-
-          break;
-
-        case TITLE:
-          if (IsKeyPressed (KEY_ENTER))
-            currentScreen = GAMEPLAY;
-          break;
-
-        case GAMEPLAY:
-          if (IsKeyPressed (KEY_ENTER))
-            currentScreen = ENDING;
-          break;
-        }
+      squarePositionCenter.y += GetMouseWheelMove () * factorSpeed;
 
       BeginDrawing ();
       ClearBackground (RAYWHITE);
-
-      switch (currentScreen)
-        {
-        case LOGO:
-          DrawText ("THE SCAM COMPANY", 10, 10, 12, RED);
-          DrawText (TextFormat ("TIEMPO TRANSCURRIDO: %i", (int)GetTime ()),
-                    10, 22, 12, RED);
-          break;
-
-        case TITLE:
-          DrawRectangle (0, 0, screenWidth, screenWidth, PURPLE);
-          DrawText ("WELCOME 2D JUNGLE", 10, 10, 10, GOLD);
-          DrawText ("THE FURY CITY", 10, 20, 10, GOLD);
-          DrawText ("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220,
-                    20, GOLD);
-          break;
-
-        case GAMEPLAY:
-          DrawRectangle (0, 0, screenWidth, screenWidth, DARKGRAY);
-          DrawText ("LETS GET READY", 10, 10, 10, WHITE);
-          DrawText ("READY PLAYER ONE...", 10, 20, 10, WHITE);
-          DrawText ("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 120, 220,
-                    20, WHITE);
-          DrawCircleV (ballPosition, 50, ballColor);
-          break;
-
-        case ENDING:
-          DrawRectangle (0, 0, screenWidth, screenHeight, YELLOW);
-          DrawText ("GAME OVER", 10, 10, 10, PINK);
-          DrawText ("PRESS ESC TO EXIT THE GAME", 10, 20, 10, PINK);
-          break;
-
-          DrawText ("Hola!!!", 190, 200, 20, LIGHTGRAY);
-        }
+      DrawRectangleV (squarePositionCenter, { squareSide, squareSide },
+                      MAROON);
+      DrawText ("Use mouse wheel to move the cube up and down!", 5, 5, 20,
+                GRAY);
+      DrawText (
+          TextFormat ("Box position Y: %03d", (int)squarePositionCenter.y), 5,
+          30, 20, LIGHTGRAY);
       EndDrawing ();
     }
 
